adfde122a6cea4af161a32da040a2a3f43397599
Manuel Cifuentes <manucifu@iesjandula.es>
2021-08-25 18:49:37 +0200

    deshabilitamos la precompilacion

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,5 +57,5 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  config.assets.digest = false
+  config.assets.initialize_on_precompile = false
 end
